76d223287df317a7e3aba3d365e75dfa64b2621d
Alice <achan520@terpmail.umd.edu>
2023-04-16 19:26:17 -0400

    lab 8

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -80,8 +80,18 @@ function getRandomIntInclusive(min, max) {
 
   function markerPlace(array, map){
     console.log('array for markers', array);
+
+    map.eachLayer((layer) => {
+        if (layer instanceof L.Marker) {
+          layer.remove();
+        }
+      });
+
+
     array.forEach((item) => {
         console.log('markerPlace', item);
+        const {coordinates} = item.geocoded_column_1.coordinates;
+        L.marker([coordinates[1], coordinates[0]]).addTo(map);
     })
   }
   
@@ -89,6 +99,7 @@ function getRandomIntInclusive(min, max) {
     const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
     // Add a querySelector that targets your filter button here
     const loadDataButton = document.querySelector('#data_load');
+    const clearDataButton = document.querySelector('#data_clear');
     const generateListButton = document.querySelector('#generate');
     const textField = document.querySelector('#resto');
   
@@ -99,8 +110,8 @@ function getRandomIntInclusive(min, max) {
     const carto = initMap();
   
     const storedData = localStorage.getItem('storedData');
-    const parsedData = JSON.parse(storedData);
-    if (parsedData.length > 0){
+    let parsedData = JSON.parse(storedData);
+    if (parsedData?.length > 0){
       generateListButton.classList.remove("hidden");
     }
 
@@ -130,7 +141,11 @@ function getRandomIntInclusive(min, max) {
        // This changes the response from the GET into data we can use - an "object"
       const storedList = await results.json();
       localStorage.setItem('storedData', JSON.stringify(storedList));
+      parsedData = storedList;
       // Make sure generate button toggles properly when load button is clicked
+      if (parsedData?.length > 0){
+        generateListButton.classList.remove("hidden");
+      }
       
       loadAnimation.style.display = 'none';
       //console.table(storedList); /*This array initially contains all 1,000 records from your request,*/
@@ -152,6 +167,12 @@ function getRandomIntInclusive(min, max) {
         console.log(newList);
         injectHTML(newList);
         markerPlace(newList, carto);
+    });
+
+    clearDataButton.addEventListener("click", (event) => {
+        console.log('clear browser data');
+        localStorage.clear();
+        console.log('localStorage check', localStorage.getItem("storedData"));
     })
   }
   function processRestaurants(list) {
@@ -235,5 +256,5 @@ function getRandomIntInclusive(min, max) {
     It's calling the 'mainEvent' function at line 57
     It runs first because the listener is set to when your HTML content has loaded
   */
-  document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+  document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
   
