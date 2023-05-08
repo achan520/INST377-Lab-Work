26e08f2458513d2ad6acb194adec160aa48e2595
Alice <achan520@terpmail.umd.edu>
2023-05-07 22:17:28 -0400

    lab6

--- scripts.js
+++ scripts.js
@@ -209,3 +209,61 @@ function getRandomIntInclusive(min, max) {
   */
   document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
   
+
+
+
+
+  const generateListButton = document.getElementById('generate');
+const restaurant_list = document.getElementById('restaurant_list');
+
+
+
+const restaurantSearch = document.getElementById('restaurant_search');
+let allRestaurants = [];
+generateListButton.addEventListener('click', () => {
+    fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?restaurant=${encodeURIComponent(resto)}')
+    .then(response => response.json())
+    .then(data => {
+      if (data !== undefined && data !== null && typeof data === 'object') {
+        allRestaurants = Object.keys(data);
+        displayRestaurants(allRestaurants);
+      } else {
+        console.error('Invalid data received from API');
+      }
+  })
+});
+
+
+
+
+
+
+
+
+// Function to display a list of breeds
+function displayRestaurants(restaurants) {
+  // Clear any existing list items
+  restaurant_list.innerHTML = '';
+
+  // Loop through the breeds and add a new list item for each breed
+  for (const restaurant of restaurants) {
+    const listItem = document.createElement('li');
+    listItem.textContent = restaurant;
+    restaurant_list.appendChild(listItem);
+  }
+}
+
+// Add an event listener to the search box
+restaurantSearch.addEventListener('input', () => {
+  const searchString = restaurantSearch.value.trim().toLowerCase();
+  const filteredRestaurants = allRestaurants.filter(restaurant => restaurant.toLowerCase().includes(searchString));
+  displayRestaurants(filteredRestaurants);
+});
+
+
+
+
+
+
+
+
