316d74be40030ad49b8063ce7699e9a66b9273ee
Alice <achan520@terpmail.umd.edu>
2023-05-07 22:38:29 -0400

    lab6

--- scripts.js
+++ scripts.js
@@ -240,25 +240,7 @@ generateListButton.addEventListener('click', () => {
 
 
 
-// Function to display a list of breeds
-function displayRestaurants(restaurants) {
-  // Clear any existing list items
-  restaurant_list.innerHTML = '';
 
-  // Loop through the breeds and add a new list item for each breed
-  for (const restaurant of restaurants) {
-    const listItem = document.createElement('li');
-    listItem.textContent = restaurant;
-    restaurant_list.appendChild(listItem);
-  }
-}
-
-// Add an event listener to the search box
-restaurantSearch.addEventListener('input', () => {
-  const searchString = restaurantSearch.value.trim().toLowerCase();
-  const filteredRestaurants = allRestaurants.filter(restaurant => restaurant.toLowerCase().includes(searchString));
-  displayRestaurants(filteredRestaurants);
-});
 
 
 
