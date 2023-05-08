246976f8b4241a9a69e59f1aa7ce2030fff7a7d9
Alice <achan520@terpmail.umd.edu>
2023-05-07 21:02:01 -0400

    lab6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -41,12 +41,12 @@ loadButton.addEventListener('click', () => {
 
 
 // Function to display a list of breeds
-function displayRestaurants(restaurants) {
+function displayRestaurants(Restaurant) {
   // Clear any existing list items
   restaurant_list.innerHTML = '';
 
   // Loop through the breeds and add a new list item for each breed
-  for (const restaurant of restaurants) {
+  for (const restaurant of Restaurant) {
     const listItem = document.createElement('li');
     listItem.textContent = restaurant;
     restaurant_list.appendChild(listItem);
