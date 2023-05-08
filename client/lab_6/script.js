f1c1a93c840448bc0cd78467a88cb69541fadd19
Alice <achan520@terpmail.umd.edu>
2023-05-07 22:31:10 -0400

    lab6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -8,13 +8,13 @@ const restaurantSearch = document.getElementById('restaurant_search');
 let allRestaurants = [];
 generateListButton.addEventListener('click', () => {
   fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json?restaurant=$')
-  .then(response => response.json())
-  .then(data => {
-    if (data !== undefined && data !== null && typeof data === 'object') {
-      allRestaurants = Object.keys(data);
-      displayRestaurants(allRestaurants);
+    .then(response => response.json())
+    .then(data => {
+      if (data !== undefined && data !== null && typeof data === 'object') {
+        allRestaurants = Object.keys(data);
+        displayRestaurants(allRestaurants);
     } else {
-      console.error('Invalid data received from API');
+        console.error('Invalid data received from API');
     }
 })
 });
