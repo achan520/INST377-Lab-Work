37766666fde1ff8d01a60c6c32321ecdf6cce3f8
Alice <achan520@terpmail.umd.edu>
2023-05-07 20:18:46 -0400

    lab6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,6 +1,8 @@
 const generateListButton = document.getElementById('generate');
 const restaurant_list = document.getElementById('restaurant_list');
 
+
+
 const restaurantSearch = document.getElementById('restaurant_search');
 let allRestaurants = [];
 generateListButton.addEventListener('click', () => {
@@ -13,6 +15,22 @@ generateListButton.addEventListener('click', () => {
 });
 
 
+
+//Get data from API and load into the collection
+fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json')
+  .then(response => response.json())
+  .then(data => {
+    if (data.status === 'success') {
+      const dogs = Object.keys(data.message);
+      collection.data = dogs;
+      collection.save(); // Save the collection to localStorage
+      console.log('Dog breeds data loaded and stored in the collection.');
+    } else {
+      console.log('Failed to retrieve dog breeds data from API.');
+    }
+  });
+
+
 // Function to display a list of breeds
 function displayRestaurants(restaurants) {
   // Clear any existing list items
@@ -20,7 +38,7 @@ function displayRestaurants(restaurants) {
 
   // Loop through the breeds and add a new list item for each breed
   for (const restaurant of restaurants) {
-    const listItem = document.createElement('ol');
+    const listItem = document.createElement('li');
     listItem.textContent = restaurant;
     restaurant_list.appendChild(listItem);
   }
